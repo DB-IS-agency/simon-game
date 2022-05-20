fc973de39aeb1c20624793f7c203c3103a86ca03
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-20 14:29:14 -0400

    📦 NEW: Add functionality to check user's answer

--- game.js
+++ game.js
@@ -15,18 +15,24 @@ $(document).keydown(function() {
 $(".btn").click(function() {
   let userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
 
   playSound(userChosenColor);
   animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
 });
 
 function nextSequence() {
+  // reset the userClickedPattern for the next level
+  userClickedPattern = [];
+
   level++;
   $("#level-title").text(`Level ${level}`);
 
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+  console.log(gamePattern);
 
   // jQuery to select button with same id as randomChosenColor
   $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100); // animate a flash to the button
@@ -45,4 +51,27 @@ function animatePress(currentColor) {
   setTimeout(function() {
     $(`#${currentColor}`).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    // call the next color in pattern after a short delay
+    if(userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }else{
+    console.log("wrong");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200)
+
+    $("h1").text("Game Over!, Press Any Key to Restart");
+  }
 }
